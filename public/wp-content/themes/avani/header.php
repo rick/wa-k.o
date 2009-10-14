d748e3ef905bad6dd6dc7e08a2b076c20d830ad7
Rick Bradley <rick@rickbradley.com>
2009-10-14 21:11:38 +0530

    first passable style/php/functions to emulate static avani site in wordpress

--- public/wp-content/themes/avani/header.php
+++ public/wp-content/themes/avani/header.php
@@ -1,45 +1,42 @@
 <?php
 /**
  * @package WordPress
- * @subpackage Default_Theme
+ * @subpackage Avani_Theme
  */
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
-
-<head profile="http://gmpg.org/xfn/11">
+<head>
+  <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
-<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
-
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-
-<style type="text/css" media="screen">
-
-<?php
-// Checks to see whether it needs a sidebar or not
-if ( empty($withcomments) && !is_single() ) {
-?>
-	#page { background: url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbg-<?php bloginfo('text_direction'); ?>.jpg") repeat-y top; border: none; }
-<?php } else { // No sidebar ?>
-	#page { background: url("<?php bloginfo('stylesheet_directory'); ?>/images/kubrickbgwide.jpg") repeat-y top; border: none; }
-<?php } ?>
-
-</style>
-
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
 <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-<div id="page">
-
-
-<div id="header" role="banner">
-	<div id="headerimg">
-		<h1><a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a></h1>
-		<div class="description"><?php bloginfo('description'); ?></div>
-	</div>
-</div>
-<hr />
+<table width="770" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#FFFFFF"> 
+  <tr> 
+    <td><table width="770" height="600" border="0" cellspacing="0" cellpadding="0"> 
+        <tr> 
+          <td width="170" valign="top"><a href="/" class="menu"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/logo2.gif" alt="about us" width="170" height="212" border="0"></a></td> 
+          <td width="600" valign="top"><table width="600" border="0" cellspacing="0" cellpadding="0"> 
+              <tr> 
+                <td height="187" valign="top" background="<?php bloginfo('stylesheet_directory'); ?>/images/line4.gif">
+                  <div align="center">
+                    <a href="products.htm" class="menu"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/banner.jpg" width="468" height="60" border="0"></a> 
+                  </div> 
+                  <div class="menubar"> 
+                    <ul>
+                      <?php wp_list_pages('sort_column=menu_order&title_li='); ?>
+                    </ul>
+                  </div> 
+                  <div class="submenubar"> 
+                    <ul>
+                      <?php wp_list_pages('sort_column=menu_order&title_li='); ?>
+                    </ul>
+                  </div> 
+                </td>
+              </tr> 
