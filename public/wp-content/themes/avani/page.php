3570bc00e07435179cf4e75ab11be5b703eac0b6
Rick Bradley <rick@rickbradley.com>
2009-10-16 09:55:40 +0530

    make site map work again, presumably more robustly now; shame this is PHP

--- public/wp-content/themes/avani/page.php
+++ public/wp-content/themes/avani/page.php
@@ -4,25 +4,33 @@
  * @subpackage Avani_Theme
  */
 
-get_header(); ?>
+get_header();
+sitemap_area($post);
 
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-  <?php sitemap_area($post); ?>
+if (have_posts()) {
+  while (have_posts()) : the_post(); ?>
   <div class="content">
-    <?php 
-    if($post->post_tiale == 'Site Map') {?>
+    <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>');
+    if ($post->post_title == 'Site Map') { ?>
     <ul role="navigation">
     	<?php wp_list_pages('title_li='); ?>
     </ul>
     <?php 
-    } else { 
-     the_content('<p class="serif">Read the rest of this page &raquo;</p>');
-    } ?>
+    } 
+    ?>
   </div>
-<?php endwhile; endif; ?>
-
-
+<?php 
+  endwhile;
+} else {
+?>
+  <div class="content">
+    <ul role="navigation">
+    	<?php wp_list_pages('title_li='); ?>
+    </ul>
+  </div>
+  <?php 
+}
+?>
 
 <?php get_search_form(); ?>
-
 <?php get_footer(); ?>
